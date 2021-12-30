66a5beda7430ec782f0377ed5300967930ff226b
Tenzin Thabkhae <tenzin.thabkhae@mail.utoronto.ca>
2021-12-30 15:38:00 -0500

    hmmmm gh-ppages branch problems

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,10 @@ git add -A
 git commit -m 'deploy'
 
 git checkout --orphan gh-pages
-
+git rm -rf .
+mv dist/* . &&  rmdir dist/
+git add assets/ favicon.ico index.html
+git commit -m 'deploy' --no-verify.
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
