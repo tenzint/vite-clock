4cdad819f661de43f212c1120aa4603c592c4380
Tenzin Thabkhae <tenzin.thabkhae@mail.utoronto.ca>
2021-12-30 01:03:59 -0500

    vite.config.ts - base updated with '/vite-clock/' deploy.sh - creating gh-pages branch

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,8 @@ git init
 git add -A
 git commit -m 'deploy'
 
+git checkout --orphan gh-pages
+
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
