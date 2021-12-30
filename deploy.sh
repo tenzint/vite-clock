6f2efa603b7f8caecbfe6ea5fe78b89023742cf5
Tenzin Thabkhae <tenzin.thabkhae@mail.utoronto.ca>
2021-12-30 15:43:48 -0500

    changes based on a tutorial

--- deploy.sh
+++ deploy.sh
@@ -6,25 +6,14 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
-cd dist
+git add dist -f
+git commit -m 'deploy dist for production'
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-git checkout --orphan gh-pages
-git rm -rf .
-mv dist/* . &&  rmdir dist/
-git add assets/ favicon.ico index.html
-git commit -m 'deploy' --no-verify.
+git subtree push --prefix dist origin gh-pages
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:tenzint/vite-clock.git master:gh-pages
+# git push -f git@github.com:tenzint/vite-clock.git master:gh-pages
 
-cd -
+# cd -
